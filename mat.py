3254e775b79b22ad029df9827c594941b27c9d3e
MikeL Morgan <smokymorgan@gmail.com>
2013-08-05 00:37:22 -0700

    started mat.py

--- mat.py
+++ mat.py
@@ -3,12 +3,13 @@ from vec import Vec
 def getitem(M, k):
     "Returns the value of entry k in M.  The value of k should be a pair."
     assert k[0] in M.D[0] and k[1] in M.D[1]
-    pass
+    #print(M.f)
+    return M.f[k] if k in M.f else 0
 
 def setitem(M, k, val):
     "Sets the element of v with label k to be val.  The value of k should be a pair"
     assert k[0] in M.D[0] and k[1] in M.D[1]
-    pass
+    M.f[k]= val
 
 def add(A, B):
     "Returns the sum of A and B"
